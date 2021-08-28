fad369a27dd809f1901390a62dad42404477f91e
Eduardo Pina <mrejpm@gmail.com>
2021-08-28 18:11:54 -0400

    [Add] inventory cache functions

--- src/hooks/useInventory.js
+++ src/hooks/useInventory.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import useDolarOptions from './useDolarOptions';
 import useGetRequest from './useGetRequest';
 import processInventoryElement from '../utils/processInventoryElement';
+import Storage from '../libs/storage';
 
 const useInventory = (modalActions) => {
   const [inventory, setInventory] = useState([]);
@@ -11,11 +12,31 @@ const useInventory = (modalActions) => {
   const baseInventory=useGetRequest('https://inpimaca-api.herokuapp.com/inventario');
   const dolarOptions = useDolarOptions();
 
-  useEffect(()=>{
-    if (baseInventory){
+  useEffect(async () => {
+    const stringInventory = await Storage.instance.get('inventory')
+    setInventory(stringInventory ? JSON.parse(stringInventory) : []);
+  } , [])
 
+  useEffect(async () => {
+    const stringDolarOptions = await Storage.instance.get('dolarOptions')
+    const processedInventory = inventory.map(element => processInventoryElement(element, JSON.parse(stringDolarOptions), modalActions))
+    dipatch({
+      type:'SET_INVENTORY',
+      payload:processedInventory
+    })
+    dipatch({
+      type:'SET_DOLAR_OPTIONS',
+      payload:JSON.parse(stringDolarOptions)
+    })
+  } , [inventory])
+
+  useEffect(async ()=>{
+    if (baseInventory){
+      const stringInventory = JSON.stringify(baseInventory)
+      await Storage.instance.store('inventory',stringInventory)
+      const stringDolarOptions = JSON.stringify(dolarOptions);
+      await Storage.instance.store('dolarOptions', stringDolarOptions);
       const processedInventory = baseInventory.map(element => processInventoryElement(element, dolarOptions, modalActions))
-      setInventory(processedInventory);
       dipatch({
         type:'SET_INVENTORY',
         payload:processedInventory
