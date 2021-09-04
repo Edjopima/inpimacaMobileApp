5774e4841bc53f41a5f97f0bb8248826fb310197
Eduardo Piña <mrejpm@gmail.com>
2021-09-04 12:27:15 -0400

    [Fix] build script

--- buildScript.sh
+++ buildScript.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/bash
-
-build_project {
-    # Build the project
-    echo "Building project..."
-    cd android && ./gradlew assembleRelease
-    cd ..
-    echo 'Builded'
+build_project () {
+  # Build the project
+  echo "Building project..."
+  cd android && ./gradlew assembleRelease
+  cd ..
+  echo 'Builded'
 }
-
-gen_apk {
-    # Generate the apk
-    echo "Generating apk..."
-    java -jar "C:\Users\mrejp\Documents\dev\bundletool-all-1.8.0.jar" build-apks --bundle="C:\Users\mrejp\Documents\dev\inpimacaMobileApp\android\app\build\outputs\bundle\release\app-release.aab" --output="C:\Users\mrejp\Documents\dev\inpimacaMobileApp\android\app\build\outputs\bundle\release\releaseapk.apks" --mode=universal --ks="C:\Users\mrejp\Documents\dev\inpimacaMobileApp\android\app\inpimaca-app.keystore" --ks-key-alias=inpimaca-app
-    echo 'Generated'
+gen_apk () {
+  # Generate the apk
+  echo "Generating apk..."
+  java -jar "C:\Users\mrejp\Documents\dev\bundletool-all-1.8.0.jar" build-apks --bundle="C:\Users\mrejp\Documents\dev\inpimacaMobileApp\android\app\build\outputs\bundle\release\app-release.aab" --output="C:\Users\mrejp\Documents\dev\inpimacaMobileApp\android\app\build\outputs\bundle\release\releaseapk.apks" --mode=universal --ks="C:\Users\mrejp\Documents\dev\inpimacaMobileApp\android\app\inpimaca-app.keystore" --ks-key-alias=inpimaca-app
+  echo 'Generated'
 } 
-
 echo "Starting build..."
 build_project
 gen_apk
